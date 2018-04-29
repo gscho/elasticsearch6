64350932561c41ada256ca66cddcb7ce2a52f0af
GSCHOF11 <gregs@indellient.com>
2018-04-29 08:53:25 -0400

    Elasticsearch habitat plan

---
+++ habitat/plan.sh
@@ -0,0 +1,47 @@
+pkg_name=elasticsearch
+pkg_origin=gscho
+pkg_version=6.2.2
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_description="Open Source, Distributed, RESTful Search Engine"
+pkg_upstream_url="https://elastic.co"
+pkg_license=('Revised BSD')
+pkg_source=https://artifacts.elastic.co/downloads/${pkg_name}/${pkg_name}-${pkg_version}.tar.gz
+pkg_shasum=b26e3546784b39ce3eacc10411e68ada427c5764bcda3064e9bb284eca907983
+pkg_deps=(
+  core/coreutils-static
+  core/glibc
+  core/jre8
+)
+pkg_bin_dirs=(es/bin)
+pkg_binds_optional=(
+  [elasticsearch]="http-port transport-port"
+)
+pkg_lib_dirs=(es/lib)
+pkg_exports=(
+  [http-port]=network.port
+  [transport-port]=transport.port
+)
+pkg_exposes=(http-port transport-port)
+
+
+do_build() {
+  return 0
+}
+
+do_install() {
+  install -vDm644 README.textile "${pkg_prefix}/README.textile"
+  install -vDm644 LICENSE.txt "${pkg_prefix}/LICENSE.txt"
+  install -vDm644 NOTICE.txt "${pkg_prefix}/NOTICE.txt"
+
+  # Elasticsearch is greedy when grabbing config files from /bin/..
+  # so we need to put the untemplated config dir out of reach
+  mkdir -p "${pkg_prefix}/es"
+  cp -a ./* "${pkg_prefix}/es"
+
+  # jvm.options needs to live relative to the binary.
+  # mkdir -p "$pkg_prefix/es/config"
+  # install -vDm644 config/jvm.options "$pkg_prefix/es/config/jvm.options"
+
+  # Delete unused binaries to save space
+  rm "${pkg_prefix}/es/bin/"*.bat "${pkg_prefix}/es/bin/"*.exe
+}
